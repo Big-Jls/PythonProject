aaa021e6f8c8e5273d93bc15d2b0ba51ac8d0eb5
Big-Jls <921447735@qq.com>
2025-04-01 20:52:21 +0800

    day2

--- day2/作业/hdbz.py
+++ day2/作业/hdbz.py
@@ -10,16 +10,16 @@ m_group = m.group(1)
 # 缩略图
 m1 = re.findall('data-original="(.*?)"',m_group, re.S)
 # print(m1)
-# for img_thumbs_url in m1:
-#     response = requests.get(img_thumbs_url)
-#     match = re.search(r'/(\d+)\.(jpg|png)', img_thumbs_url)
-#     if match:
-#         img_id = match.group(1)
-#         img_ext = match.group(2)
-#         with open(f'./images/thumbs/{img_id}.{img_ext}', 'wb') as f:
-#             f.write(response.content)
-#         print(f"成功保存图片: {img_id}.{img_ext}")
-#         time.sleep(0.5)
+for img_thumbs_url in m1:
+    response = requests.get(img_thumbs_url)
+    match = re.search(r'/(\d+)\.(jpg|png)', img_thumbs_url)
+    if match:
+        img_id = match.group(1)
+        img_ext = match.group(2)
+        with open(f'./images/thumbs/{img_id}.{img_ext}', 'wb') as f:
+            f.write(response.content)
+        print(f"成功保存图片: {img_id}.{img_ext}")
+        time.sleep(0.5)
 
 # 预览图
 m2 = re.findall('href="(.*?)"',m_group, re.S)
